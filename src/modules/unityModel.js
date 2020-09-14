c37d460f1340d81fcb09dea1280538d09fcf6657
ismailmri <mohammad.ismail9190@gmail.com>
2020-09-14 14:21:08 +0300

    build

--- src/modules/unityModel.js
+++ src/modules/unityModel.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
 
+
+if (typeof window === 'undefined')
 const unityContext = new UnityContext({
     loaderUrl: "/placeObj/Build/placeObj.loader.js",
     dataUrl: "/placeObj/Build/placeObj.data",
