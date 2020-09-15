c232f4046ce611bd98e5d603acdca26a15828b5a
ismailmri <mohammad.ismail9190@gmail.com>
2020-09-15 14:53:55 +0300

    added unity model

--- src/modules/unityModel.js
+++ src/modules/unityModel.js
@@ -1,14 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
 
-
-if (typeof window === 'undefined')
-const unityContext = new UnityContext({
-    loaderUrl: "/placeObj/Build/placeObj.loader.js",
-    dataUrl: "/placeObj/Build/placeObj.data",
-    frameworkUrl: "/placeObj/Build/placeObj.framework.js",
-    codeUrl: "/placeObj/Build/placeObj.wasm",
-});
+    const unityContext = new UnityContext({
+        loaderUrl: "/placeObj/Build/placeObj.loader.js",
+        dataUrl: "/placeObj/Build/placeObj.data",
+        frameworkUrl: "/placeObj/Build/placeObj.framework.js",
+        codeUrl: "/placeObj/Build/placeObj.wasm",
+    });
 
  const UnityModel = () => {
     
